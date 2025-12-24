9befa2ee2565495148fbcc1531e0802ada143f33
JohnMichaelJarvis <John_Michael_Jarvis@protonmail.com>
2025-12-24 13:18:34 -0500

    Feat: Implement user input handling and response token tracking

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import argparse
 import os
 from google import genai
+from google.genai import types
 from dotenv import load_dotenv
 
 
@@ -7,18 +9,30 @@ def main():
     load_dotenv()
 
     api_key = os.environ.get("GEMINI_API_KEY")
-    print(api_key)
     if api_key == None:
         raise RuntimeError("API Key missing...")
 
     client = genai.Client(api_key=api_key)
 
-    response = client.models.generate_content(
-        model="gemini-2.5-flash",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.",
-    )
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="User prompt")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
 
-    print(response.text)
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+
+    response = client.models.generate_content(
+        model="gemini-2.5-flash", contents=messages
+    )
+    if response.usage_metadata == None:
+        raise RuntimeError("API response usage metadata is empty")
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    response_tokens = response.usage_metadata.candidates_token_count
+    if args.verbose:
+        print(f"User prompt: {args.user_prompt}")
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {response_tokens}")
+    print(f"Response:\n{response.text}")
 
 
 if __name__ == "__main__":
